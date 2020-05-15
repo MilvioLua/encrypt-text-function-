273c09e64fd85ccfbdfee8d1d65e811277aebe65
Milvio LuA <milviolua@gmail.com>
2020-05-15 17:31:25 +0200

    added uncrypt
    added uncrypt

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-def encryption ( phrase):
+def encryption (phrase):
     encrypt = ""
     for letter in phrase:
         if letter in "AEIOUaeiou":
             encrypt = encrypt + "g"
+        elif letter in "ml":
+            encrypt = encrypt + "6"
+
         else:
             encrypt = encrypt + letter
     return encrypt
 
 print(encryption(input("Enter a phrase")))
 
+def descryption (descryptphrase):
+
+    sword = 12
+    sphrase = "milvio"
+    descrypt = ""
+
+    for letter in descryptphrase:
+        if descryptphrase == sword:
+            print(sphrase)
+        else:
+            print("worng enter again")
+
+    return descrypt
+print(descryption(input("Enter description")))
